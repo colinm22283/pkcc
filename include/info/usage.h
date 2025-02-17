fc01bc5b2ba9eef8f1e9c60b182711d73563adc7
Colin McGann <colinm22@iastate.edu>
2025-02-17 15:17:42 -0600

    Updated usage

--- include/info/usage.h
+++ include/info/usage.h
@@ -6,5 +6,5 @@
     "  -d                     Enable partial debug messages\n" \
     "  -dd                    Enable full debug messages\n"    \
     "  -0, -v,   version      Print version information\n"     \
-    "  -h,   help             Print usage information"         \
+    "  -h,   help             Print usage information\n"         \
     "  -o <out_path>          Output to path <out_path>"
