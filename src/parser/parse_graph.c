c8821bc3e831127206c7a72c0f23216d41c9c1c1
Colin McGann <colinm22@iastate.edu>
2025-11-17 00:53:46 -0600

    Test commit

--- src/parser/parse_graph.c
+++ src/parser/parse_graph.c
@@ -1,13 +1,10 @@
 #include <parser/parse_graph.h>
 
 void parse_graph_init(parse_graph_t * parse_graph) {
-
 }
 
 void parse_graph_free(parse_graph_t * parse_graph) {
-
 }
 
 void parse_graph_load(parse_graph_t * parse_graph) {
-    
 }
