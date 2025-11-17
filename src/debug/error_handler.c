07f34d330937e47f469f24c67136a62dc6fcdff8
Colin McGann <colinm22@iastate.edu>
2025-11-17 01:07:39 -0600

    Added ansi to error_handler.c

--- src/debug/error_handler.c
+++ src/debug/error_handler.c
@@ -4,16 +4,18 @@
 
 #include <main.h>
 #include <options.h>
+
+#include <debug/ansi.h>
 #include <debug/error_handler.h>
 
 __NORETURN void fatal_error(const char * restrict fmt, ...) {
     va_list args;
     va_start(args, fmt);
 
-    if (options.color_enable) fprintf(stderr, "\033[31;1mERROR:\n");
+    if (options.color_enable) fprintf(stderr, A_FG_RED A_BOLD "ERROR:\n");
     else fprintf(stderr, "ERROR:\n");
     vfprintf(stderr, fmt, args);
-    if (options.color_enable) fprintf(stderr, "\033[0m");
+    if (options.color_enable) fprintf(stderr, A_RESET);
 
     va_end(args);
 
